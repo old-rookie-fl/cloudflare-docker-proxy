65c78ba981c3301ab12812049987241789b21fea
old-rookie-fl <83766273+old-rookie-fl@users.noreply.github.com>
2024-06-27 17:13:09 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.fliang.top": dockerHub,
+  "quay.fliang.top": "https://quay.io",
+  "gcr.fliang.top": "https://gcr.io",
+  "k8s-gcr.fliang.top": "https://k8s.gcr.io",
+  "k8s.fliang.top": "https://registry.k8s.io",
+  "ghcr.fliang.top": "https://ghcr.io",
+  "cloudsmith.fliang.top": "https://docker.cloudsmith.io",
+  "ecr.fliang.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.fliang.top": dockerHub,
 };
 
 function routeByHosts(host) {
